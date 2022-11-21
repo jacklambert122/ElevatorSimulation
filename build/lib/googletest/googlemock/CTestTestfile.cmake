cb1a07bffef2025af3bcd90ab6678af184bb9a7f
jacklambert <jack122lambert@yahoo.com>
2022-11-21 14:30:44 -0500

    Remove build

--- build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jacklambert/Docs/VScode Projects/Elevator Simulation/lib/googletest/googlemock
-# Build directory: /Users/jacklambert/Docs/VScode Projects/Elevator Simulation/build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jacklambert/Docs/VScode Projects/Elevator Simulation/lib/googletest/googletest
-# Build directory: /Users/jacklambert/Docs/VScode Projects/Elevator Simulation/build/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
